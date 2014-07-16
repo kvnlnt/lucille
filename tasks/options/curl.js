34e2d42861d1d5fc04b73cf6e32afc6651e41fb4
kvnlnt <kvnlnt@yahoo.com>
2014-07-15 17:46:00 -0700

    build cleanup

--- tasks/options/curl.js
+++ tasks/options/curl.js
@@ -1,7 +1,7 @@
 module.exports = {
 	'libs/jquery.js'   : 'http://code.jquery.com/jquery-2.1.1.min.js',
-	'libs/lodash.js'   : 'https://raw.github.com/lodash/lodash/2.4.1/dist/lodash.compat.min.js',
-	'libs/snap.svg.js' : 'https://raw.githubusercontent.com/adobe-webplatform/Snap.svg/master/dist/snap.svg-min.js',
+	'libs/lodash.js'   : 'https://raw.githubusercontent.com/kvnlnt/lodash/master/dist/lodash.compat.min.js',
+	'libs/snap.js'     : 'https://raw.githubusercontent.com/kvnlnt/Snap.svg/master/dist/snap.svg-min.js',
     'libs/pickl.js'    : 'https://raw.githubusercontent.com/kvnlnt/pickl/master/dist/pickl.min.js',
     'libs/pickl.css'   : 'https://raw.githubusercontent.com/kvnlnt/pickl/master/dist/pickl.min.css',
 }
