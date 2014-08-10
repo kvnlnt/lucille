257d75eb84aa27ab05afa044c340775911a30637
kvnlnt <kvnlnt@yahoo.com>
2014-08-10 16:04:33 -0700

    replaced libs with submodules

--- tasks/options/curl.js
+++ tasks/options/curl.js
@@ -1,11 +1,2 @@
-module.exports = {
-	
-	'libs/jquery.js'   : 'http://code.jquery.com/jquery-2.1.1.min.js',
-	'libs/lodash.js'   : 'https://raw.githubusercontent.com/kvnlnt/lodash/master/dist/lodash.compat.min.js',
-	'libs/snap.js'     : 'https://raw.githubusercontent.com/kvnlnt/Snap.svg/master/dist/snap.svg-min.js',
-	'libs/teoria.js'   : 'https://raw.githubusercontent.com/kvnlnt/teoria/master/dist/teoria.min.js',
-	'libs/howler.js'   : 'https://raw.githubusercontent.com/kvnlnt/howler.js/master/howler.min.js',
-	'libs/hammer.js'   : 'https://raw.githubusercontent.com/kvnlnt/hammer.js/master/hammer.min.js'
-
-}
+module.exports = {}
 
