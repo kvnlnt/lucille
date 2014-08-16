7def0668739d33ca24c9380c355be675ec8c426a
kvnlnt <kvnlnt@yahoo.com>
2014-08-16 15:41:20 -0700

    usemin conversion

--- tasks/options/concat.js
+++ tasks/options/concat.js
@@ -1,23 +1,5 @@
 module.exports = {
     options: {
       separator: ';',
-    },
-    dist: {
-      src: ['scripts/lucille.js', 
-      		'scripts/fixtures.js',
-			'scripts/instruments.js',
-			'scripts/calculations.js',
-			'scripts/renders.js',
-			'scripts/displays.js',
-			'scripts/play.js',
-			'scripts/updates.js',
-			'scripts/translators.js',
-			'scripts/getters.js',
-            'scripts/trackers.js',
-			'scripts/destroyers.js',
-			'scripts/themes.js',
-			'scripts/configurations.js',
-			'scripts/themes.js'],
-      dest: 'dist/lucille.js',
-    },
+    }
 }
--- tasks/options/curl.js
+++ tasks/options/curl.js
@@ -1,2 +1,8 @@
-module.exports = {}
-
+module.exports = {
+    'vendor/jquery.js' : 'http://code.jquery.com/jquery-2.1.1.min.js',
+    'vendor/lodash.js' : 'https://raw.githubusercontent.com/kvnlnt/lodash/master/dist/lodash.compat.min.js',
+    'vendor/snap.js'   : 'https://raw.githubusercontent.com/kvnlnt/Snap.svg/master/dist/snap.svg-min.js',
+    'vendor/teoria.js' : 'https://raw.githubusercontent.com/kvnlnt/teoria/master/dist/teoria.min.js',
+    'vendor/howler.js' : 'https://raw.githubusercontent.com/kvnlnt/howler.js/master/howler.min.js',
+    'vendor/hammer.js' : 'https://raw.githubusercontent.com/kvnlnt/hammer.js/master/hammer.min.js'
+}
